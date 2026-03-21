073d8a5caa29553079664f3d9284c741fed00263
karthikeyanwxs-art <karthikeyanwxs@gmail.com>
2026-03-21 17:54:03 +0530

    01.HALF_ADDER.v

--- COMBINATIONAL/01.HALF_ADDER.v
+++ COMBINATIONAL/01.HALF_ADDER.v
@@ -8,3 +8,20 @@ module tb();
   wire s,c;
 
   
+module tb();
+  reg a,b;
+  wire d,bo;
+  halfsub aa(a,b,d,bo);
+  initial begin
+    $monitor("@ %0t the input a=%b b=%b the output d=%b bo=%b",$time,a,b,d,bo);
+    a=1'b0;b=1'b0;
+    #10 a=1'b0;b=1'b1;
+    #10 a=1'b1;b=1'b0;
+    #10 a=1'b1;b=1'b1;
+    #10 $finish;
+  end
+  initial begin 
+    $dumpfile("halfsub.vcd");
+    $dumpvars(0,tb); 
+  end
+endmodule
